0a9483281be98ccf22379762ed361eb72f462f9e
Whanos <github@sergal.fun>
2022-09-01 14:27:08 +0100

    started api

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![forbid(unsafe_code)]
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hopper!"
+}
+
+#[catch(404)]
+fn not_found_general() -> &'static str {
+    "Not Found"
+}
+
+#[catch(400)]
+fn bad_request() -> &'static str {
+    "Bad Request"
+}
+
+#[post("/", format = "json")]
+fn log_error() -> String {
+    // TODO: log this somewhere
+    "Logged".to_string()
+}
+
+#[launch]
+fn rocket() -> _ { // Ignore the error on the underscore. It works fine, trust me.
+    rocket::build()
+        .mount("/", routes![index, log_error])
+        .register("/", catchers![not_found_general, bad_request])
+}
