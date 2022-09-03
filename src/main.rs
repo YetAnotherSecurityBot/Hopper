0c1afa4e124fef2ba798ee66c68451e6ee499218
Whanos <github@sergal.fun>
2022-09-03 16:43:27 +0100

    started things

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![forbid(unsafe_code)]
 #[macro_use]
 extern crate rocket;
+use rocket::serde::{Deserialize, json::Json};
+use std::net::SocketAddr;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -17,16 +19,31 @@ fn bad_request() -> &'static str {
     "Bad Request"
 }
 
-#[post("/", format = "json")]
-fn log_error() -> String {
+#[post("/", format = "json", data="<data>")]
+fn log(data: Json<LogData>, remote_addr: SocketAddr) -> String {
     // TODO: log this somewhere
     "Logged".to_string()
+}
+
+#[get("/metrics")]
+fn metrics() -> String {
+    format!("e")
 }
 
 #[launch]
 fn rocket() -> _ {
     // Ignore the error on the underscore. It works fine, trust me.
     rocket::build()
-        .mount("/", routes![index, log_error])
+        .mount("/", routes![index, log, metrics])
         .register("/", catchers![not_found_general, bad_request])
+}
+
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+struct LogData {
+    log_type: LogType
+}
+
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+enum LogType {
+    PFP_COMPARISON_RESULT
 }
