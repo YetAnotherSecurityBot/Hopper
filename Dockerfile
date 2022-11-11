779c386a20bd300a8289da37a4e47a515f8bf9e2
Whanos <github@sergal.fun>
2022-11-11 04:55:49 +0000

    made it build for linux + updated metrics page + stuff

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,29 @@
-FROM rust:1.31
+FROM rust:1.60 as build
 
-WORKDIR /usr/src/myapp
-COPY . .
+# create a new empty shell project
+RUN USER=root cargo new --bin hopper
+WORKDIR /hopper
 
-RUN cargo install --path .
+# copy over your manifests
+COPY ./Cargo.lock ./Cargo.lock
+COPY ./Cargo.toml ./Cargo.toml
 
-CMD ["hopper"]
+# this build step will cache your dependencies
+RUN cargo build --release
+RUN rm src/*.rs
+
+# copy your source tree
+COPY ./src ./src
+
+# build for release
+RUN rm ./target/release/deps/hopper*
+RUN cargo build --release
+
+# our final base
+FROM rust:1.60-slim-buster
+
+# copy the build artifact from the build stage
+COPY --from=build /hopper/target/release/hopper .
+
+# set the startup command to run your binary
+CMD ["./hopper"]
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 #![forbid(unsafe_code)]
 #[macro_use]
 extern crate rocket;
+
+use std::fmt::format;
 use rocket::serde::{Deserialize, json::Json};
 use std::net::SocketAddr;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+use crate::LogType::{JOINED_SERVER, LEFT_SERVER, PFP_COMPARISON_RESULT, NEW_MESSAGE};
+
+lazy_static! {
+    static ref PFP_COMPARISON_RESULT_COUNTER: Mutex<i32> = Mutex::new(0);
+    static ref JOINED_SERVER_COUNTER: Mutex<i32> = Mutex::new(0);
+    static ref LEFT_SERVER_COUNTER: Mutex<i32> = Mutex::new(0);
+    static ref NEW_MESSAGE_COUNTER: Mutex<i32> = Mutex::new(0);
+}
 
 #[get("/")]
 fn index() -> &'static str {
@@ -22,12 +34,29 @@ fn bad_request() -> &'static str {
 #[post("/", format = "json", data="<data>")]
 fn log(data: Json<LogData>, remote_addr: SocketAddr) -> String {
     // TODO: log this somewhere
+    if data.log_type == PFP_COMPARISON_RESULT {
+        *PFP_COMPARISON_RESULT_COUNTER.lock().unwrap() += 1;
+    }
+    if data.log_type == JOINED_SERVER {
+        *JOINED_SERVER_COUNTER.lock().unwrap() += 1;
+    }
+    if data.log_type == LEFT_SERVER {
+        *LEFT_SERVER_COUNTER.lock().unwrap() += 1;
+    }
+    if data.log_type == NEW_MESSAGE {
+        *NEW_MESSAGE_COUNTER.lock().unwrap() += 1;
+    }
     "Logged".to_string()
 }
 
 #[get("/metrics")]
 fn metrics() -> String {
-    format!("e")
+    let mut result = String::new();
+    result.push_str("# HELP yasb_pfp_comparisons_total The number of times a pfp comparison result was logged\n");
+    result.push_str(format!("yasb_pfp_comparisons_total {}\n", PFP_COMPARISON_RESULT_COUNTER.lock().unwrap()).as_str());
+    result.push_str("# HELP yasb_new_messages\n");
+    result.push_str(format!("yasb_new_messages {}\n", NEW_MESSAGE_COUNTER.lock().unwrap()).as_str());
+    result
 }
 
 #[launch]
@@ -40,10 +69,14 @@ fn rocket() -> _ {
 
 #[derive(Debug, PartialEq, Eq, Deserialize)]
 struct LogData {
-    log_type: LogType
+    log_type: LogType,
+    log_message: String
 }
 
 #[derive(Debug, PartialEq, Eq, Deserialize)]
 enum LogType {
-    PFP_COMPARISON_RESULT
+    PFP_COMPARISON_RESULT,
+    JOINED_SERVER,
+    LEFT_SERVER,
+    NEW_MESSAGE,
 }
